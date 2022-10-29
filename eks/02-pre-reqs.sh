b1407d65557a9aa4d033ac21b70c0ed9eebc88b2
awsandy <awsandy@amazon.co.uk>
2022-10-29 18:48:42 +0100

    up

--- eks/02-pre-reqs.sh
+++ eks/02-pre-reqs.sh
@@ -1,9 +1,12 @@
 # metrics server
 # same as 
-kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.5.0/components.yaml
+#kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.5.0/components.yaml
 #
-kubectl apply -f kpack
-kubectl apply -f contour
-kubectl apply -f service-binding/servicebinding-runtime-v*.yaml
-kubectl -n servicebinding-system rollout status deployment/servicebinding-controller-manager --watch=true
-kubectl apply -f service-binding/servicebinding-workloadresourcemappings-v*.yaml"
+#kubectl apply -f kpack
+#kubectl apply -f contour
+#kubectl apply -f service-binding/servicebinding-runtime-v*.yaml
+#kubectl -n servicebinding-system rollout status deployment/servicebinding-controller-manager --watch=true
+#kubectl apply -f service-binding/servicebinding-workloadresourcemappings-v*.yaml"
+cd scripts
+./install-dependacies.sh
+
---
+++ eks/03-helm-korifi.sh
@@ -0,0 +1,5 @@
+export ROOT_NAMESPACE="cf"
+export KORIFI_NAMESPACE="korifi-system"
+export ADMIN_USERNAME="cf-admin"
+export BASE_DOMAIN="example.com"
+helm install korifi https://github.com/cloudfoundry/korifi/releases/download/v0.4.0/korifi-0.4.0.tgz --create-namespace --namespace="$KORIFI_NAMESPACE"     --set=global.generateIngressCertificates=true     --set=global.rootNamespace="$ROOT_NAMESPACE"     --set=adminUserName="$ADMIN_USERNAME"     --set=api.apiServer.url="api.$BASE_DOMAIN"     --set=global.defaultAppDomainName="apps.$BASE_DOMAIN"     --set=api.packageRepositoryPrefix=europe-west1-docker.pkg.dev/my-project/korifi/packages     --set=kpack-image-builder.builderRepository=europe-west1-docker.pkg.dev/my-project/korifi/kpack-builder     --set=kpack-image-builder.dropletRepositoryPrefix=europe-west1-docker.pkg.dev/my-project/korifi/droplets
